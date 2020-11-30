f77666cdb24a9c0f2ae1b8ca6cef172ab52437ce
AliceSolda <75255398+AliceSolda@users.noreply.github.com>
2020-11-30 17:19:26 +0100

    Update IEADB_treaties_text.py

--- IEA_texts/IEADB_treaties_text.py
+++ IEA_texts/IEADB_treaties_text.py
@@ -3,33 +3,27 @@ import urllib.request
 import re
 from bs4 import BeautifulSoup
 
-
 #to run this code, you first need to create a folder where you will
 # store the treaties texts. In this example, I created a folder 
 #called "treaties" on my desktop.
 
-list_text_id = [
+list_treaty = [
     3059,
     8501,
     3100,
-    5006] #add all your treaties IDs in there.
+    5006] #add all treaties IDs in there
 
-
-i = 0
-n = len(list_text_id) #with n the total number of treaties.
-
-while i <= n-1:  
-    treaty_id = list_text_id[i]
-    url_string='https://iea.uoregon.edu/treaty-text/'+str(treaty_id)
+for treaty in list_treaty: 
+    url_string='https://iea.uoregon.edu/treaty-text/'+str(treaty)
     print(url_string) #print the URL to test that the code is working
     
     website_url=requests.get(url_string)
 
     soup = BeautifulSoup(website_url.text,'lxml')
-    myDiv = soup.find('div', {'class':'content clearfix'})
-    myText = str(myDiv)  
+    html_treaty = soup.find('div', {'class':'content clearfix'})
+    text_treaty = str(html_treaty)  
 
-    filename = "text_"+str(list_text_id[i])
+    filename = "text_"+str(treaty)
     print(filename) #print the file name to test that the code is working
     location = "C:/Users/alice/Desktop/treaties/" + filename + ".doc"
     file = open(location, "w", encoding="utf-8")
@@ -38,9 +32,6 @@ while i <= n-1:
     #"clean" text in our word document.
     
     cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', myText)
+    cleantext = re.sub(cleanr, '', text_treaty)
     file.write(cleantext)    
-    
     file.close()
-
-    i += 1
