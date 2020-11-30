6d10699eb74b4bab9b6cd6716f127e2f09266960
AliceSolda <75255398+AliceSolda@users.noreply.github.com>
2020-11-30 16:28:27 +0100

    Add files via upload

---
+++ IEA_texts/IEADB_treaties_text.py
@@ -0,0 +1,48 @@
+import requests
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+
+
+#to run this code, you need to create a folder where you are going to store
+#the treaties' text.
+
+list_text_id = [3059, 8501, 3100, 5006] #add all agreements id in there
+
+
+i = 0
+inc = 0
+n = len(list_text_id) #with n the total number of agreements
+
+while i <= n-1:  
+    treaty_id = list_text_id[inc]
+    url_string='https://iea.uoregon.edu/treaty-text/'+str(treaty_id)
+    print(url_string) #print the URL to test that the code is working
+    
+    website_url=requests.get(url_string)
+
+    soup = BeautifulSoup(website_url.text,'lxml')
+    myDiv = soup.find('div', {'class':'content clearfix'})
+    mytext = myDiv.findAll('p') #creates a list of all the "p"s   
+
+    
+    filename = "text_"+str(list_text_id[i])
+    print(filename)
+    location = "C:/Users/alice/Desktop/treaties/" + filename + ".doc"
+    file = open(location, "w", encoding="utf-8")
+
+    #Now we are going to write on our .doc file each element (define by the tag <p>) from our list
+    #(mytext). But before adding a line to the .doc file, we are going to clean it from its html tag. 
+    
+    for line in mytext: 
+        new_line = str(line)
+        cleanr = re.compile('<.*?>') 
+        cleantext = re.sub(cleanr, '', new_line) #clean the text from the html tag
+        cleantext = cleantext + "\n"
+        file.write(cleantext)
+    
+    file.close()
+
+
+    i += 1
+    inc += 1
