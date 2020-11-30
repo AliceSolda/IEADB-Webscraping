d09ebaaa3adc0a6cb8e3af7a1c9a667ac1e4e7df
AliceSolda <75255398+AliceSolda@users.noreply.github.com>
2020-11-30 16:39:36 +0100

    Update IEADB_treaties_text.py

--- IEA_texts/IEADB_treaties_text.py
+++ IEA_texts/IEADB_treaties_text.py
@@ -4,15 +4,19 @@ import re
 from bs4 import BeautifulSoup
 
 
-#to run this code, you need to create a folder where you are going to store
-#the treaties' text.
+#to run this code, you first need to create a folder where you will
+# store the treaties texts.
 
-list_text_id = [3059, 8501, 3100, 5006] #add all agreements id in there
+list_text_id = [
+    3059,
+    8501,
+    3100,
+    5006] #add all treaties IDs in there
 
 
 i = 0
 inc = 0
-n = len(list_text_id) #with n the total number of agreements
+n = len(list_text_id) #with n the total number of treaties
 
 while i <= n-1:  
     treaty_id = list_text_id[inc]
@@ -23,26 +27,22 @@ while i <= n-1:
 
     soup = BeautifulSoup(website_url.text,'lxml')
     myDiv = soup.find('div', {'class':'content clearfix'})
-    mytext = myDiv.findAll('p') #creates a list of all the "p"s   
+    myText = str(myDiv)  
 
-    
     filename = "text_"+str(list_text_id[i])
-    print(filename)
+    print(filename) #print the file name to test that the code is working
     location = "C:/Users/alice/Desktop/treaties/" + filename + ".doc"
     file = open(location, "w", encoding="utf-8")
 
     #Now we are going to write on our .doc file each element (define by the tag <p>) from our list
     #(mytext). But before adding a line to the .doc file, we are going to clean it from its html tag. 
     
-    for line in mytext: 
-        new_line = str(line)
-        cleanr = re.compile('<.*?>') 
-        cleantext = re.sub(cleanr, '', new_line) #clean the text from the html tag
-        cleantext = cleantext + "\n"
-        file.write(cleantext)
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', myText)
+    cleantext = cleantext + "\n"
+    file.write(cleantext)    
     
     file.close()
-
 
     i += 1
     inc += 1
