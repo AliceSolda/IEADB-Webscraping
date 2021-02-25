ea065dd4a67b891257095ba3c0672391920f72bd
Alice Soldà, PhD <75255398+AliceSolda@users.noreply.github.com>
2021-02-25 14:02:59 +0100

    Add files via upload

---
+++ IEA_texts/IAEDB_treaties_text.py
@@ -0,0 +1,34 @@
+import requests
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+
+#to run this code, you first need to create a folder where you will
+# store the treaties texts. In this example, I created a folder 
+#called "treaties" on my desktop.
+
+list_treaty = [
+    
+#add all treaties IDs in there
+
+    ]
+
+for treaty in list_treaty: 
+    url_string='https://iea.uoregon.edu/treaty-text/'+str(treaty)
+    print(url_string) #print the URL to test that the code is working
+    
+    website_url=requests.get(url_string)
+
+    soup = BeautifulSoup(website_url.text,'lxml')
+    text_treaty = soup.find('div', {'class':'content clearfix'}).get_text()
+
+    filename = "text_"+str(treaty)
+    print(filename) #print the file name to test that the code is working
+    location = "C:/Users/alice/Desktop/treaties/" + filename + ".doc"
+    file = open(location, "w", encoding="utf-8")
+
+    #Now we are going to clean our text from its HTML tags and write the 
+    #"clean" text in our word document.
+    
+    file.write(str(text_treaty))    
+    file.close()
---
+++ IEA_texts/IEADB_full_page_treaties_text.py
@@ -0,0 +1,26 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+list_treaty = [
+    
+4284,
+
+    ]
+
+for treaty in list_treaty: 
+    url_string='https://iea.uoregon.edu/treaty-text/'+str(treaty)
+    print(url_string) #print the URL to test that the code is working
+    
+    website_url=requests.get(url_string).text
+
+    soup = BeautifulSoup(website_url,'lxml')
+
+    filename = "text_"+str(treaty)
+    print(filename) #print the file name to test that the code is working
+    location = "C:/Users/alice/Desktop/treaties/" + filename + ".doc"
+    file = open(location, "w", encoding="utf-8")
+
+    file.write(str(soup))    
+    file.close()
